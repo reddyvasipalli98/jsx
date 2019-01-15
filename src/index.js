08e37f8523e6e16093694abc29536335ac775137
DESKTOP-FE4RJJG\reddy <reddyvasipalli98@gmail.com>
2019-01-14 22:04:37 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Import the react adn reactdom
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//Create a react component
+const App = () => {
+    return <div>sample</div>;
+}
+
+//Take the react component and show it on the screen
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
